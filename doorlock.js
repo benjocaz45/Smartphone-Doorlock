8562210fe96aaf15d24a6cbfeeac33204a7517c5
benjocaz45 <40012850+benjocaz45@users.noreply.github.com>
2018-06-07 10:50:06 -0400

    Update doorlock.js

--- doorlock.js
+++ doorlock.js
@@ -21,10 +21,6 @@
 // The GPIO pin the signal wire on your button
 // is connected to. It is okay to have no button connected
 //
-// Parameters: ledPin
-// The GPIO pin the signal wire on your led
-// is connected to. It is okay to have no ledconnected
-//
 // Parameter: blynkToken
 // The token which was generated for your blynk
 // project
@@ -34,17 +30,17 @@ var unlockedState = 1000;
 var lockedState = 2200;
 
 var motorPin = 14;
-var buttonPin = 4
-var OLledPin = 17 //Outside Locked (red) led indicator
-var OUledPin = 27 //Outside UNLocked (green) led indicator
-var ILledPin = 23 //Inside Locked (red) led indicator
-var IUledPin = 18 //Inside UNLocked (green) led indicator
+var buttonPin = 4;
+var OLledPin = 17; //Outside Locked (red) led indicator
+var OUledPin = 27; //Outside UNLocked (green) led indicator
+var ILledPin = 23; //Inside Locked (red) led indicator
+var IUledPin = 18; //Inside UNLocked (green) led indicator
 
-var blynkToken = 'blynk-auth-token';
+var blynkToken = 'your-auth-token';
 
 // *** Start code *** //
 
-var locked = true
+var locked = true;
 
 //Setup servo
 var Gpio = require('pigpio').Gpio,
@@ -62,17 +58,18 @@ var Gpio = require('pigpio').Gpio,
 var Blynk = require('blynk-library');
 var blynk = new Blynk.Blynk(blynkToken);
 var v0 = new blynk.VirtualPin(0);
+var v1 = new blynk.VirtualPin(1);
 
-console.log("locking door")
-lockDoor()
+console.log("locking door");
+lockDoor();
 
 button.on('interrupt', function (level) {
-	console.log("level: " + level + " locked: " + locked)
+	console.log("level: " + level + " locked: " + locked);
 	if (level == 0) {
 		if (locked) {
-			unlockDoor()
+			unlockDoor();
 		} else {
-			lockDoor()
+			lockDoor();
 		}
 	}
 });
@@ -80,9 +77,9 @@ button.on('interrupt', function (level) {
 v0.on('write', function(param) {
 	console.log('V0:', param);
   	if (param[0] === '0') { //unlocked
-  		unlockDoor()
+  		unlockDoor();
   	} else if (param[0] === '1') { //locked
-  		lockDoor()
+  		lockDoor();
   	} else {
   		blynk.notify("Door lock button was pressed with unknown parameter");
   	}
@@ -91,6 +88,17 @@ v0.on('write', function(param) {
 blynk.on('connect', function() { console.log("Blynk ready."); });
 blynk.on('disconnect', function() { console.log("DISCONNECT"); });
 
+//v1.on('write', function() {
+//    if (locked) {
+//        ILled.digitalWrite(1);
+//        setTimeout(function(){ILled.digitalWrite(0)}, 2000);
+//    } else { //if unlocked
+//        IUled.digitalWrite(1); //sets inside unlock indicator (green) high
+//        setTimeout(function(){IUled.digitalWrite(0)}, 2000); //sets inside unlock indicator (green) low after 2 seconds
+//    }
+//setTimeout(function(){blynk.virtualWrite(v1, 0)},1000);
+//});
+
 function lockDoor() {
 	motor.servoWrite(lockedState);
 	OLled.digitalWrite(1); //Sets outside lock indicator (red) high
@@ -98,7 +106,7 @@ function lockDoor() {
 	ILled.digitalWrite(1); //Sets inside lock indicator (red) high
 	IUled.digitalWrite(0); //Sets inside unlock indicator (green) low
 	setTimeout(function(){ILled.digitalWrite(0)}, 2000); //Waits 2 seconds, then sets inside lock indicator (red) low
-	locked = true
+	locked = true;
 
 	//notify
   	blynk.notify("Door has been locked!");
@@ -109,12 +117,12 @@ function lockDoor() {
 
 function unlockDoor() {
 	motor.servoWrite(unlockedState);
-	OLled.digitalWrite(0);
-	OUled.digitalWrite(1);
-	ILled.digitalWrite(0);
-	IUled.digitalWrite(1);
-	setTimeout(function(){IUled.digitalWrite(0)});
-	locked = false
+	OLled.digitalWrite(0); //Sets outside lock indicator (red) low
+	OUled.digitalWrite(1); //Sets outside unlock indicator (green) high
+	ILled.digitalWrite(0); //Sets inside lock indocator (red) low
+	IUled.digitalWrite(1); //Sets inside unlock indicator (green) high
+	setTimeout(function(){IUled.digitalWrite(0)}, 2000); //shuts off inside unlock indicator (green) low after 2 seconds
+	locked = false;
 
 	//notify
   	blynk.notify("Door has been unlocked!"); 
